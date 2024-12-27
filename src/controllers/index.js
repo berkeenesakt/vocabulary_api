ca6a9e713f836d1d3ec0c64b39ccd9049ee4bfe8
İsmail Hakkı Dovanay <ihdovanay@outlook.com>
2024-12-27 14:27:06 +0300

    Initialize Express application with routing and middleware setup

---
+++ src/controllers/index.js
@@ -0,0 +1,28 @@
+// This file exports various controller functions that handle requests and responses for different API endpoints.
+
+const getItems = (req, res) => {
+    // Logic to get items
+    res.send('Get items');
+};
+
+const createItem = (req, res) => {
+    // Logic to create an item
+    res.send('Create item');
+};
+
+const updateItem = (req, res) => {
+    // Logic to update an item
+    res.send('Update item');
+};
+
+const deleteItem = (req, res) => {
+    // Logic to delete an item
+    res.send('Delete item');
+};
+
+module.exports = {
+    getItems,
+    createItem,
+    updateItem,
+    deleteItem
+};
